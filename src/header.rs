cd2f3fa43c1e1dbf5491bcb4c8a48cfb1819c2e6
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-04 12:03:58 +0530

    src: headers[tokenize headers + header parsing] line_tokens

--- src/header.rs
+++ src/header.rs
@@ -1,1 +1,177 @@
-// Headers
+use charset::Charset;
+
+use crate::find_from;
+
+pub enum HeaderToken<'a> {
+    Text(&'a str),
+    Whitespace(&'a str),
+    Newline(Option<String>),
+    DecodeWord(String),
+}
+
+// boundaries
+fn is_boundary(line: &str, ix: Option<usize>) -> bool {
+    ix.and_then(|v| line.chars().nth(v))
+        .map(|c| {
+            c.is_whitespace()
+                || c = '"'
+                || c = ')'
+                || c = '('
+                || c = '<'
+                || c = '>'
+                || c = ','
+        })
+        .unwrap_or(true)
+}
+
+fn decode_word(encoded: &str) -> Option<String> {
+    let ix_delim1 = encoded.find('?');
+    let ix_delim2 = find_from(encoded, ix_delim1 + 1, "?")?;
+
+    let charset = &encoded[0..ix_delim1];
+    let transfer_coding = &encoded[ix_delim1 + 1..ix_delim2];
+    let input = &encoded[ix_delim2 + 1..];
+
+    let decoded = match transfer_coding {
+        "B" | "b" => data_encoding::BASE64_MIME.decode(input.as_bytes()).ok()?
+        "Q" | "q" => {
+            let to_decode = input.replace("_", " ");
+            let trimmed = to_decode.trim_end();
+            let mut d = quoted_printable::decode(trimmed, quoted_printable::ParseMode::Robust);
+            if d.is_ok() && to_decode.len() != trimmed.len() {
+                d.as_mut()
+                    .unwrap()
+                    .extend_from_slice(to_decode[trimmed.len()..].as_bytes());
+            }
+            d.ok()?
+        }
+        _ => return None,
+    };
+
+    let charset = Charset::for_label_no_replacement(charset.as_bytes())?;
+    let (cow, _) = charset.decode_without_bom_handling(&decode);
+    Some(cow.into_owned())
+}
+
+// Tokenizes a single line of the header and proces a vector of tokens.
+fn tokenize_header_line(line: &str) -> Vec<Headertoken> {
+    fn maybe_whitespace(text: &str) -> HeaderToken {
+        if text.trim_end().is_empty() {
+            HeaderToken::Whitespace(text)
+        } else {
+            HeaderToken::Text(text)
+        }
+    }
+
+    let mut result = Vec::new();
+    let mut ix_search = 0;
+    loop {
+        match find_from(line, ix_search, "=?") {
+            Some(v) => {
+                let ix_begin = v + 2;
+                if !is_boundary(line, ix_begin.check_sub(3)) {
+                    result.push(HeaderToken::Text(&line[ix_search..ix_begin]));
+                    ix_search = ix_begin;
+                    continue;
+                }
+
+                result.push(maybe_whitespace(&line[ix_search..ix_begin - 2]));
+                let mut ix_end_search = ix_begin;
+                loop {
+                    match find_from(line, ix_end_search, "?=") {
+                        Some(ix_end) => {
+                            if !is_boundary(line, ix_end.checked_add(2)) {
+                                ix_end_search = ix_end  + 2;
+                                continue;
+                            }
+                            match decode_word(&line[ix_begin..ix_end]) {
+                                Some(v) => result.push(HeaderToken::DecodedWord(v));
+                                None => {
+                                    result.push(HeaderToken::Text(&line[ix_begin - 2..ix_end + 2]));
+                                }
+                            };
+                            ix_search = ix_end;
+                        }
+                        None => {
+                            result.push(Headertoken::Text("=?"));
+                            ix_search = ix_begin - 2;
+                        }
+                    };
+                    break;
+                }
+                ix_search += 2;
+                continue;
+            }
+        }
+    }
+}
+
+fn tokenize_header(value: &str) -> Vec<HeaderToken> {
+    let mut tokens = Vec::new();
+    let mut lines = value.lines();
+    let mut first = true;
+
+    while let Some(line) = lines.next().map(str::trim_start) {
+        if first {
+            first = false; 
+        } else {
+            tokens.push(HeaderToken::Newline(None));
+        }
+
+        let mut line_tokens = tokenize_header(line);
+        tokens.append(&mut line_tokens);
+    }
+
+    tokens
+}
+
+fn normalize_header_whitespace(tokens: Vec<HeaderToken>) -> Vec<HeaderToken> {
+    let mut result = Vec::<HeaderToken>::new();
+    let mut saved_token = None;
+
+    for tok in tokens {
+        match &tok {
+            HeaderToken::Text(_) => {
+                if let Some(HeaderToken::Whitespace(_)) => &saved_token {
+                    result.push(saved_token.unwrap());
+                } else if let Some(HeaderToken::Newline(Some(_))) = &saved_token {
+                    result.push(saved_token.unwrap());
+                }
+
+                result.push(tok);
+                saved_token = None;
+            }
+
+            HeaderToken::Whitespace(_) => {
+                if let Some(HeaderToken::DecodedWord(_)) = saved_token {
+                    saved_token = Some(tok);
+                } else {
+                    result.push(tok);
+                    saved_token = None;
+                }
+            }
+
+            HeaderToken::Newline(_) => {
+                if let Some(HeaderToken::Whitespace(ws)) = saved_token {
+                    let new_ws = ws.to_owned() + " ";
+                    saved_token = Some(HeaderToken::Newline(Some(new_ws)));
+                } else if let Some(HeaderToken::DecodeWord(_)) = saved_token {
+                    saved_token = Some(HeaderToken::Newline(Some(" ".to_string())));
+                } else {
+                    result.push(HeaderToken::Newline(Some(" ".to_string())));
+                    saved_token = None;
+                }
+            }
+
+            HeaderToken::DecodedWord(_) => {
+                saved_token = Some(HeaderToken::DecodeWord(String::new()));
+                result.push(tok);
+            }
+        }
+    }
+    result
+}
+
+pub fn normalized_tokens(raw_value: &str) -> Vec<HeaderToken> {
+    normalize_header_whitespace(tokenize_header(raw_value))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,102 @@
+#![forbid(unsafe_code)]
+
+extern crate charset;
+extern crate data_encoding;
+extern crate quoted_printable;
+
+use std::borrow::Cow;
+use std::collections::{BTreeMap, HashMap};
+use std::error;
+use std::fmt;
 use charset::{decode_latin1, Charset};
-// code goes here...
+
+mod addrparse;
+pub mod body;
+mod dateparse;
+mod header;
+pub mod headers;
+mod msgidparse;
+
+use crate::body::Body;
+
+
+#[derive(Debug)]
+pub enum MailParseError {
+    QuotedPrintableDecodeError(quoted_printable::QuotedPrintableError),
+    Base64DecodeError(data_encoding::DecodeError),
+    EncodingError(std::borrow::Cow<'static, str>),
+    Generic(&'static str)
+}
+
+impl fmt::Display for MailParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MailParseError::QuotedPrintableDecodeError(ref err) => {
+                write!(f, "QuotedPrintable decode error: {}", err)
+            }
+            MailParseError::Base64DecodeError(ref err) => write!(f, "Base64 decode error: {}", err),
+            MailParseError::EncodingError(ref err) => write!(f, "Encoding Error: {}", err),
+            MailParseError::Generic(ref description) => write!(f, "{}", description),
+        }
+    }
+}
+
+impl error::Error for MailParseError {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        match *self {
+            MailParseError::QuotedPrintableDecodeError(ref err) => Some(err),
+            MailParseError::Base64DecodeError(ref err) => Some(err),
+            _ => None,
+        }
+    }
+
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            MailParseError::QuotedPrintableDecodeError(ref err) => Some(err),
+            MailParseError::Base64DecodeError(ref err) => Some(err),
+            _ => None,
+        }
+    }
+}
+
+impl From<quoted_printable::QuotedPrintableError> for MailParseError {
+    fn from(err: quoted_printable::QuotedPrintableError) -> MailParseError {
+        MailParseError::QuotedPrintableDecodeError(err)
+    }
+}
+
+impl From<data_encoding::DecodeError> for MailParseError {
+    fn from(err: data_encoding::DecodeError) -> MailParseError {
+        MailParseError::Base64DecodeError(err)
+    }
+}
+
+impl From<std::borrow::Cow<'static, str>> for MailParseError {
+    fn from(err: std::borrow::Cow<'static, str>) -> MailParseError {
+        MailParseError::EncodingError(err)
+    }
+}
+
+pub struct MailHeader<'a> {
+    key: &'a[u8],
+    value: &'a[u8],
+}
+
+impl<'a> fmt::Debug for MailHeader<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MailHeader")
+            .field("key", &String::from_utf8_lossy(self.key))
+            .field("value", &String::from_utf8_lossy(self.value))
+            .finish()
+    }
+}
+
+// parser: find from email
+pub(crate) fn find_from(line: &str, ix_start: usize, key: &str) -> Option<usize> {
+    line[ix_start..].find(key).map(|v| ix_start + v)
+}
+
+fn find_from_u8(line: &[u8], ix_start: usize, key: &[u8]) -> Option<usize> {
+    assert!(!key.is_empty());
+    assert!(ix_start < line.len());
+}
